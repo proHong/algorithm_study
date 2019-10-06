04ffaea7da8ca0e42b2518554908b56d335e73d7
HSH <tjdgus0454@gmail.com>
2019-10-06 20:20:33 +0900

    [UPDATE]BOJ10953

---
+++ Baekjoon/Baekjoon[IO]/BOJ10953.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int num;
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++)
+	{
+		int a, b;
+		scanf("%d,%d", &a, &b);
+		printf("%d\n", a + b);
+
+	}
+	return 0;
+}
