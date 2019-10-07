76c2ecd5a77b1c2dfdce0c2e57ebfc5710d6510c
HSH <tjdgus0454@gmail.com>
2019-10-07 18:59:28 +0900

    [UPDATE]BOJ2742,BOJ2739

---
+++ Baekjoon/Baekjoon[IO]/BOJ2739.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	for (int i = 1; i < 10; i++)
+	{
+		printf("%d * %d = %d\n", num,i,num*i);
+	}
+	return 0;
+}
