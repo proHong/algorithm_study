04a0a8e8e2273236a135706cb21414240118cd52
tjdgus0454 <42308891+tjdgus0454@users.noreply.github.com>
2019-10-08 16:31:59 +0900

    [UPDATE]BOJ2445

---
+++ Baekjoon/Baekjoon[IO]/BOJ2445.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= 2*n-1; i++)
+	{
+		if (i < n)
+		{
+			for (int j = 0; j < i ; j++)
+				printf("*");
+			for (int j = 0; j < 2 * (n - i); j++)
+				printf(" ");
+			for (int j = 0; j < i; j++)
+				printf("*");
+		}
+		else
+		{
+			for (int j = 0; j < 2 * n - i; j++)
+				printf("*");
+			for (int j = 0; j < -2 * (n - i); j++)
+				printf(" ");
+			for (int j = 0; j < 2 * n - i; j++)
+				printf("*");
+		}
+
+		printf("\n");
+	}
+	return 0;
+}
