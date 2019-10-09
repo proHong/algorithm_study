687cc2f2c4b4644a8d062026b45aa09a46b3d067
tjdgus0454 <42308891+tjdgus0454@users.noreply.github.com>
2019-10-09 10:30:58 +0900

    [UPDATE]BOJ10817

---
+++ Baekjoon/Baekjoon[IO]/BOJ10817.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a[3];
+	scanf("%d %d %d", &a[0], &a[1], &a[2]);
+
+	sort(a, a + 3);
+
+	printf("%d", a[1]);
+
+	return 0;
+}
