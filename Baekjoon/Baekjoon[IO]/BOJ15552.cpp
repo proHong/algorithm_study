a9892a6c9c6890106b461330ea6f5e5353bc3958
tjdgus0454 <42308891+tjdgus0454@users.noreply.github.com>
2019-10-09 11:02:19 +0900

    [UPDATE]BOJ15552

---
+++ Baekjoon/Baekjoon[IO]/BOJ15552.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	int a, b;
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d %d", &a, &b);
+
+		printf("%d\n", a + b);
+	}
+
+
+	return 0;
+}
