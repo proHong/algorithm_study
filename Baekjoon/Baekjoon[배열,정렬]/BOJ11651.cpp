5d54242dbf36f425bfac335baf83dc03bbe95405
Hong <tjdgus0454@gmail.com>
2019-10-22 19:44:32 +0900

    [UPDATE]BOJ11651

---
+++ Baekjoon/Baekjoon[배열,정렬]/BOJ11651.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second == b.second)
+		return a.first < b.first;
+	else
+		return a.second < b.second;
+
+}
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	vector<pair<int, int>> v;
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		scanf("%d %d", &x, &y);
+		v.push_back(make_pair(x, y));
+	}
+	sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		printf("%d %d\n", v[i].first, v[i].second);
+	}
+
+	return 0;
+}
