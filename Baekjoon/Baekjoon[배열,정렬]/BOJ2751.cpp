c74dea552210e7a74bd2d8944dfeff13a1fb0c2d
Hong <tjdgus0454@gmail.com>
2019-10-22 14:13:59 +0900

    [UPDATE]BOJ2751

---
+++ Baekjoon/Baekjoon[배열,정렬]/BOJ2751.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int *temp = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &temp[i]);
+	}
+	sort(temp, temp + n);
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\n", temp[i]);
+	}
+	delete[] temp;
+	return 0;
+}
