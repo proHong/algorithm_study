6b29f4d8b4ef50458ba4ef6d322ccc7ae796ddfc
Hong <tjdgus0454@gmail.com>
2019-10-22 19:27:07 +0900

    [UPDATE]BOJ1181

--- Baekjoon/Baekjoon[배열,정렬]/BOJ2751.cpp
+++ Baekjoon/Baekjoon[배열,정렬]/BOJ2751.cpp
@@ -1,21 +1,37 @@
 #include <algorithm>
 #include <stdio.h>
+#include <string>
+#include <vector>
 using namespace std;
+
+bool cmp(string a, string b)
+{
+	if (a.size() == b.size())
+		return a < b;
+	else
+		return a.size() < b.size();
+	
+}
+
 
 int main()
 {
 	int n;
 	scanf("%d", &n);
-	int *temp = new int[n];
+	vector<string> v;
 	for (int i = 0; i < n; i++)
 	{
-		scanf("%d", &temp[i]);
+		char temp[51];
+		scanf("%s", &temp);
+		v.push_back(temp);
 	}
-	sort(temp, temp + n);
-	for (int i = 0; i < n; i++)
+	sort(v.begin(), v.end(), cmp);
+	v.erase(unique(v.begin(), v.end()),v.end());
+
+	for (int i = 0; i < v.size(); i++)
 	{
-		printf("%d\n", temp[i]);
+		printf("%s\n", v[i].c_str());
 	}
-	delete[] temp;
+
 	return 0;
 }
